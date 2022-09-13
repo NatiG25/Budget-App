ae8d9afd3537da907bebc1940956779078d96cd5
NatiG <nati.g5684@gmail.com>
2022-09-14 01:24:00 +0300

    Add a name field for new users signing up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    before_action :update_allowed_parameters, if: :devise_controller?
+    protect_from_forgery with: :null_session,
+                         if: proc { |c| c.request.format =~ %r{application/json} }
+  
+    def after_sign_out_path_for(_resource)
+      request.referrer
+    end
+  
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :current_password)
+      end
+    end
 end
