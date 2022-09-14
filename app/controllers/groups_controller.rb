1504f215aaf5614fe035969c62138e7b3a771e36
NatiG <nati.g5684@gmail.com>
2022-09-14 17:36:57 +0300

    Create a new group method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,44 @@
 class GroupsController < ApplicationController
   load_and_authorize_resource
+  before_action :set_group, only: %i[show delete]
+  before_action :authenticate_user!, only: %i[new create destroy]
+
   def index
+    @groups = Group.all
+  end
+  
+  def show; end
+
+  def new
+    @user = User.find(params[:user_id])
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    if @group.save
+      redirect_to user_groups_path(current_user.id)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group.destroy
+
+    redirect_to user_groups_path(current_user.id)
+  end
+
+
+  private
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
