85a315fa0b983d5a323421639daa99652b3d40a0
NatiG <nati.g5684@gmail.com>
2022-09-15 17:20:32 +0300

    Change field amount into integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_14_185036) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_15_141803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "deals", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "name"
-    t.float "amount"
+    t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_deals_on_user_id"
